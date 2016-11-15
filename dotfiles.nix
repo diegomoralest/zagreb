1a59cf6674916f363867266bd1c5bde69f22b212
Diego Morales <dmoralest@MacBook-Pro.local>
2016-11-14 21:16:00 -0500

    Updated dotfiles version

--- dotfiles.nix
+++ dotfiles.nix
@@ -5,9 +5,9 @@ stdenv.mkDerivation (rec {
   name = "dotfiles";
   src = fetchgit {
     "url" = "https://github.com/diegomoralest/dotfiles";
-    "rev" = "22d182c50988eae10a19f20254efbc8ede37d536";
-    "date" = "2016-11-14T19:12:58-05:00";
-    "sha256" = "0aibjhx96g9g0sm91kflsd82cgd9pra3a4g0lddypcg0fqpkq3i2";
+    "rev" = "a900fceea66ab10c126fa0351be3bbe9e8704978";
+    "date" = "2016-11-14T20:51:12-05:00";
+    "sha256" = "1adinqny1883675di0d9scs1398mr725qp9s65vh12sji58x6kz3";
   };
 
   meta = {
