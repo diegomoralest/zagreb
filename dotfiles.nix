a93e1c7aeecb0fee36a798fe460bfe5884d147a7
Diego Morales <dmoralest@MacBook-Pro.local>
2016-11-14 21:12:57 -0500

    Fixed syntax error

--- dotfiles.nix
+++ dotfiles.nix
@@ -4,10 +4,10 @@ stdenv.mkDerivation (rec {
 
   name = "dotfiles";
   src = fetchgit {
-    "url": "https://github.com/diegomoralest/dotfiles",
-    "rev": "22d182c50988eae10a19f20254efbc8ede37d536",
-    "date": "2016-11-14T19:12:58-05:00",
-    "sha256": "0aibjhx96g9g0sm91kflsd82cgd9pra3a4g0lddypcg0fqpkq3i2"
+    "url" = "https://github.com/diegomoralest/dotfiles";
+    "rev" = "22d182c50988eae10a19f20254efbc8ede37d536";
+    "date" = "2016-11-14T19:12:58-05:00";
+    "sha256" = "0aibjhx96g9g0sm91kflsd82cgd9pra3a4g0lddypcg0fqpkq3i2";
   };
 
   meta = {
